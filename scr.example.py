775ae709a645842c61916213d4cfb62ef44347a8
Markus <m4k.join@outlook.com>
2022-04-07 20:54:29 +0200

    format

--- scr.example.py
+++ scr.example.py
@@ -9,4 +9,3 @@ scr_ip_host = "easy.box"
 # path to base64 router certificate - or set to False to disable certificate check (dont forget to remove the line containing the path)
 scr_router_pub_cert = True
 scr_router_pub_cert = "easycert.cer"
-
