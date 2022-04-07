342e64418f34a4d2a04bc6d2b98cd94e36420d70
Markus <m4k.join@outlook.com>
2022-04-07 20:35:35 +0200

    example secrets file

---
+++ scr.example.py
@@ -0,0 +1,11 @@
+# Secrets store
+
+scr_passw = "mysecretpassword123"
+
+scr_ip_host = "easy.box"
+# scr_ip_host = "192.168.1.1"
+
+# path to base64 router certificate - or set to False to disable certificate check (dont forget to remove the line containing the path)
+scr_router_pub_cert = True
+scr_router_pub_cert = "easycert.cer"
+
