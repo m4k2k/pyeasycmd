8308a02ef22e72e75e2aaf327c369f78b9d36c6c
Markus <m4k.join@outlook.com>
2023-04-30 22:17:56 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="pyeasycmd",
+    version="0.1",
+    description="Python EasyBox SOAP Client",
+    author="m4k2k",
+    author_email="nomail",
+    url="https://github.com/m4k2k/pyeasycmd",
+    packages=["pyeasycmd"],
+)
